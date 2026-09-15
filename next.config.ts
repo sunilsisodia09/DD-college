11340ec4dfa48ad716ca4f953c98aaaef9d503ec
sunilsisodia09 <ssisodiya9758@gmail.com>
2026-09-15 11:01:19 +0530

    Add GitHub static build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: "export",
+  reactStrictMode: true,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
