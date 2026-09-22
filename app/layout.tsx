ddb13a0fb96b560e1449faf6b96a1e3ea105ce3c
sunilsisodia09 <ssisodiya9758@gmail.com>
2026-09-22 11:46:23 +0530

    Add Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,17 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
+
 export const metadata: Metadata = {
   title: {
     default: "DD College | Admissions Open 2026",
     template: "%s | DD College",
   },
-
   description:
     "DD College Admissions 2026. Explore programs, discover opportunities and start your journey towards a successful future.",
-
-  keywords: [
-    "DD College",
-    "DD College Admission",
-    "DD College Admission 2026",
-    "College Admission 2026",
-    "Admissions Open 2026",
-    "DD College Courses",
-    "DD College Programs",
-    "DD College Dehradun",
-  ],
-
-  icons: {
-    icon: "/icons/favicon.png",
-  },
 };
 
 export default function RootLayout({
@@ -36,9 +22,39 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '2964234100588286');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
         <Navbar />
+
         <main>{children}</main>
-          <Footer/>
+
+        <Footer />
+
+        {/* Meta Pixel noscript */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2964234100588286&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </body>
     </html>
   );
